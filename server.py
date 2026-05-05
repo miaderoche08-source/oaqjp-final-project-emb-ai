e0d8564494983b3bbc6ecf9cf9407399ad07bae3
miaderoche08 <mia.deroche08@gmail.com>
2026-05-04 21:24:45 -0400

    Task 7

--- server.py
+++ server.py
@@ -5,20 +5,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    """
-    Analyzes the text provided by the user and returns the emotion scores 
-    and the dominant emotion.
-    """
-    
     text_to_analyze = request.args.get('textToAnalyze')
-
-    
     response = emotion_detector(text_to_analyze)
 
-    
+    # Task 7: Handle scenario where dominant_emotion is None
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-
 
     return (
         f"For the given statement, the system response is 'anger': {response['anger']}, "
@@ -26,7 +18,7 @@ def sent_analyzer():
         f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
         f"The dominant emotion is {response['dominant_emotion']}."
     )
-
+    
 @app.route("/")
 def render_index_page():
     """
